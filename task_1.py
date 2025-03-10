76c411b46686279bf6fe9d259e574242bba5b98d
Tuc-Bambook <grigoriy1207@gmail.com>
2025-03-10 13:51:14 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,5 +1,6 @@
 class Book:
     """ Базовый класс книги. """
+
     def __init__(self, name: str, author: str):
         self.name = name
         self.author = author
@@ -10,22 +11,58 @@ class Book:
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
+    @property
+    def name(self) -> int:
+        return self._name
 
-class PaperBook:
+    @property
+    def author(self) -> int:
+        return self._author
+
+
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.pages = pages
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"{super().__str__()}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if not isinstance(pages, int):
+            raise TypeError
+        if not pages <= 0:
+            raise ValueError
+        self._pages = pages
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.duration = duration
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"{super().__str__()}. Продолжительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float) -> None:
+        if not isinstance(duration, float):
+            raise TypeError
+        if not duration <= 0:
+            raise ValueError
+        self._duration = duration
